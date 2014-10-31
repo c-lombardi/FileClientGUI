631c64893c1169cade221b1d228f61bda86be0b3
c-lombardi <clombardi@problemsolutions.net>
2014-10-31 16:35:46 -0400

    first commit

---
+++ Cvl5FileClientApp/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Cvl5FileClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.KeyTextBox = new System.Windows.Forms.TextBox();
+            this.ValueTextBox = new System.Windows.Forms.TextBox();
+            this.ResponseTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AddBtn = new System.Windows.Forms.RadioButton();
+            this.DeleteBtn = new System.Windows.Forms.RadioButton();
+            this.EditBtn = new System.Windows.Forms.RadioButton();
+            this.GetBtn = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.GetBtn);
+            this.groupBox1.Controls.Add(this.EditBtn);
+            this.groupBox1.Controls.Add(this.DeleteBtn);
+            this.groupBox1.Controls.Add(this.AddBtn);
+            this.groupBox1.Location = new System.Drawing.Point(87, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 254);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Action Menu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(505, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Value:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(505, 346);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Response:";
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.Location = new System.Drawing.Point(574, 105);
+            this.KeyTextBox.MaxLength = 25;
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.Size = new System.Drawing.Size(159, 20);
+            this.KeyTextBox.TabIndex = 4;
+            // 
+            // ValueTextBox
+            // 
+            this.ValueTextBox.Enabled = false;
+            this.ValueTextBox.Location = new System.Drawing.Point(574, 138);
+            this.ValueTextBox.MaxLength = 25;
+            this.ValueTextBox.Name = "ValueTextBox";
+            this.ValueTextBox.Size = new System.Drawing.Size(159, 20);
+            this.ValueTextBox.TabIndex = 5;
+            // 
+            // ResponseTextBox
+            // 
+            this.ResponseTextBox.Enabled = false;
+            this.ResponseTextBox.Location = new System.Drawing.Point(574, 339);
+            this.ResponseTextBox.Name = "ResponseTextBox";
+            this.ResponseTextBox.Size = new System.Drawing.Size(159, 20);
+            this.ResponseTextBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.AutoSize = true;
+            this.AddBtn.Location = new System.Drawing.Point(62, 70);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(44, 17);
+            this.AddBtn.TabIndex = 0;
+            this.AddBtn.TabStop = true;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.CheckedChanged += new System.EventHandler(this.AddBtn_CheckedChanged);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.AutoSize = true;
+            this.DeleteBtn.Location = new System.Drawing.Point(62, 93);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(56, 17);
+            this.DeleteBtn.TabIndex = 1;
+            this.DeleteBtn.TabStop = true;
+            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.CheckedChanged += new System.EventHandler(this.DeleteBtn_CheckedChanged);
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.AutoSize = true;
+            this.EditBtn.Location = new System.Drawing.Point(62, 116);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(43, 17);
+            this.EditBtn.TabIndex = 2;
+            this.EditBtn.TabStop = true;
+            this.EditBtn.Text = "Edit";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            this.EditBtn.CheckedChanged += new System.EventHandler(this.EditBtn_CheckedChanged);
+            // 
+            // GetBtn
+            // 
+            this.GetBtn.AutoSize = true;
+            this.GetBtn.Location = new System.Drawing.Point(62, 139);
+            this.GetBtn.Name = "GetBtn";
+            this.GetBtn.Size = new System.Drawing.Size(42, 17);
+            this.GetBtn.TabIndex = 3;
+            this.GetBtn.TabStop = true;
+            this.GetBtn.Text = "Get";
+            this.GetBtn.UseVisualStyleBackColor = true;
+            this.GetBtn.CheckedChanged += new System.EventHandler(this.GetBtn_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 467);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ResponseTextBox);
+            this.Controls.Add(this.ValueTextBox);
+            this.Controls.Add(this.KeyTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox KeyTextBox;
+        private System.Windows.Forms.TextBox ValueTextBox;
+        private System.Windows.Forms.TextBox ResponseTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton GetBtn;
+        private System.Windows.Forms.RadioButton EditBtn;
+        private System.Windows.Forms.RadioButton DeleteBtn;
+        private System.Windows.Forms.RadioButton AddBtn;
+    }
+}
+
